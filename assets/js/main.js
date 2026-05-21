de1d0478aa8c2f78227231cbbe1806833a0ed4c1
Samuel Korosi <samuel.korosi@gmail.com>
2026-05-20 21:43:58 -0300

    feat: enriquecimento dos cases

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,25 @@ document.querySelectorAll('.nav a').forEach(link => {
         navToggle.classList.remove('active');
     });
 });
+
+
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
+
+document.querySelectorAll('.zoomable-img').forEach(image => {
+    image.addEventListener('click', () => {
+        modal.classList.add('active');
+        modalImage.src = image.src;
+        modalImage.alt = image.alt;
+    });
+});
+
+modal.addEventListener('click', () => {
+    modal.classList.remove('active');
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        modal.classList.remove('active');
+    }
+});
