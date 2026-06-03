5d53cdba6d00ba732008a99810a9579e7e412445
Samuel Korosi <samuel.korosi@gmail.com>
2026-06-03 09:17:02 -0300

    feat: dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,61 @@ document.addEventListener('keydown', (event) => {
         modal.classList.remove('active');
     }
 });
+
+// ======================================
+// Theme Toggle
+// ======================================
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    document.documentElement.setAttribute(
+        "data-theme",
+        savedTheme
+    );
+} else {
+    const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    ).matches;
+
+    document.documentElement.setAttribute(
+        "data-theme",
+        prefersDark ? "dark" : "light"
+    );
+}
+
+const themeToggle = document.querySelector(".theme-toggle");
+
+function updateThemeIcon() {
+    const currentTheme =
+        document.documentElement.getAttribute("data-theme");
+
+    themeToggle.textContent =
+        currentTheme === "dark"
+            ? "☀️"
+            : "🌙";
+}
+
+updateThemeIcon();
+
+themeToggle.addEventListener("click", () => {
+
+    const currentTheme =
+        document.documentElement.getAttribute("data-theme");
+
+    const newTheme =
+        currentTheme === "dark"
+            ? "light"
+            : "dark";
+
+    document.documentElement.setAttribute(
+        "data-theme",
+        newTheme
+    );
+
+    localStorage.setItem(
+        "theme",
+        newTheme
+    );
+
+    updateThemeIcon();
+});
